e2dde39a0f5b56fd5fcf634a5d9f458c8472c0bd
esekyi <sskert10@gmail.com>
2025-01-06 19:36:38 +0000

    [Fixes] Improved email task for single and batch emails

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,6 +1,3 @@
 from .celery_app import celery
-from .email_tasks import send_batch_emails_task, send_internal_email_task
-from .email_tasks import send_single_email_task
 
-__all__ = ['celery', 'send_batch_emails_task', 'send_internal_email_task',
-           'send_single_email_task']
+__all__ = ['celery']
